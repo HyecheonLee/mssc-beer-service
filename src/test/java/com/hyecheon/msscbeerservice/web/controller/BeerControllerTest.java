09f185b2d40975686f3a538cebacd81fd485ae6d
hyecheon <rainbow880616@gmail.com>
2020-01-20 23:29:09 +0900

    adding controller tests

---
+++ src/test/java/com/hyecheon/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,53 @@
+package com.hyecheon.msscbeerservice.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyecheon.msscbeerservice.web.model.BeerDto;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Test
+  void getBeerById() throws Exception {
+    mockMvc.perform(
+        get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void saveNewBeer() throws Exception {
+
+    final var beerDto = BeerDto.builder().build();
+    final var beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+    mockMvc.perform(post("/api/v1/beer")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(beerDtoJson))
+        .andExpect(status().isCreated());
+  }
+
+  @Test
+  void updateBeerById() throws Exception {
+    final var beerDto = BeerDto.builder().build();
+    final var beerDtoJson = objectMapper.writeValueAsString(beerDto);
+    mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(beerDtoJson))
+        .andExpect(status().isNoContent());
+  }
+}
