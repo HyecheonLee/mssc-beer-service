e0604ab7b8845c13e250785eedd10f93bc09f182
hyecheon <rainbow880616@gmail.com>
2020-01-20 23:09:29 +0900

    adding beer controller

---
+++ src/main/java/com/hyecheon/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package com.hyecheon.msscbeerservice.web.controller;
+
+import com.hyecheon.msscbeerservice.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+  @GetMapping("/{beerId}")
+  public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+
+    //todo impl
+    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+  }
+
+  public ResponseEntity<?> saveNewBeer(@RequestBody BeerDto beerDto) {
+    //todo impl
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity<?> updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    //todo impl
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
+
+}
